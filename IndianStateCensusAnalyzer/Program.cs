7acd1ae3141fc6b6d779bb20b9650f389a833a00
badjateamisha <amishabadjate175@gmail.com>
2022-06-11 17:15:52 +0530

    Given wrong header throws custom exception

--- IndianStateCensusAnalyzer/Program.cs
+++ IndianStateCensusAnalyzer/Program.cs
@@ -12,10 +12,13 @@ string IndianStateCensusDataWrongFilePath = @"C:\Users\amisha\source\repos\India
 string DelimiterIndianStateCensusDataFilePath = @"C:\Users\amisha\source\repos\IndianStateCensusAnalyzer\IndianStateCensus_Analyser\DelimiterIndiaStateCensusData.csv";
 string csvPath = @"C:\Users\amisha\source\repos\IndianStateCensusAnalyzerProblem\IndianStateCensusAnalyzer\IndiaStateCensusData.csv";
 //censusAnalyser.ReadCsvFile();
+
 string IndianStateCensusHeaders = "State,Population,AreaInSqKm,DensityPerSqKm";
+string IndianStateCensusHeaders2 = "States,population,areaInSqKm,densityPerSqKm";
+
 while (true)
 {
-    Console.WriteLine("Please choose the option: \n1)UC1 - Check the number of records in CSV file\n2)UC1.2 - Given wrong file Path\n3)UC1.3 - Giving wrong text file as Input\n4)UC1.4-Throw exception if Delimeter is Wrong");
+    Console.WriteLine("Please choose the option: \n1)UC1 - Check the number of records in CSV file\n2)UC1.2 - Given wrong file Path\n3)UC1.3 - Giving wrong text file as Input\n4)UC1.4-Throw exception if Delimeter is Wrong\n5)UC1.5-Throw exception if Wrong Headers are given");
     int option = Convert.ToInt32(Console.ReadLine());
     switch (option)
     {
@@ -48,6 +51,16 @@ while (true)
             {
                 IndianCensusAdapter a1 = new IndianCensusAdapter();
                 totalRecord = a1.LoadCensusData(DelimiterIndianStateCensusDataFilePath, IndianStateCensusHeaders);
+            }
+            catch (CensusAnalyserException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            break;
+        case 5:
+            try
+            {
+                totalRecord = censusAnalyser.LoadCensusData(Country.INDIA, csvPath, IndianStateCensusHeaders2);
             }
             catch (CensusAnalyserException e)
             {
